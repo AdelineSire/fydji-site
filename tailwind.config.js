c91f01d2c9b752b2a188c9d8422bc2ec7ee4557a
Adeline <adeline.sire@gmail.com>
2022-04-18 09:19:04 +0200

    Edit colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,18 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		colors: {
-			green: '#66D9C4',
-			Lightblue: '#25ddf5',
-			blue: '#0855bf',
-		},
 		fontFamily: {
 			logo: ['Pacifico', 'sans-serif'],
 			title: ['Poppins', 'sans-serif'],
 			text: ['Rubik', 'sans-serif'],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				green: '#66D9C4',
+				Lightblue: '#25ddf5',
+				blue: '#0855bf',
+			},
+		},
 	},
 	plugins: [],
 };
