d0c03fb230d48dc7a8562ddce99f6206d8145fa6
Adeline <adeline.sire@gmail.com>
2022-04-29 08:47:42 +0200

    Changes CallToAction text

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -2,11 +2,11 @@ function CallToAction() {
 	return (
 		<div className='pt-40'>
 			<h2 className='text-white font-title text-5xl font-semibold leading-relaxed mb-6'>
-				Les offres React à mon niveau
+				Les offres React
 			</h2>
 			<p className='text-white text-lg leading-normal mb-8'>
-				Recevez par email que les offres React qui vous correspondent dès leur
-				publication pour être la première personne à postuler !
+				Inscrivez-vous pour recevoir les offres d'emploi React par email tous
+				les jours !
 			</p>
 		</div>
 	);
