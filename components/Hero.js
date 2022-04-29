9bf48da28106e898e3ad124c4e40e9d7cf6bcc0d
Adeline <adeline.sire@gmail.com>
2022-04-29 08:48:56 +0200

    Improves layout

--- components/Hero.js
+++ components/Hero.js
@@ -7,19 +7,21 @@ import SubscribeForm from './SubscribeForm';
 
 function Hero() {
 	return (
-		<header className='hero-area'>
+		<header className='hero-area w-full flex flex-col items-center'>
 			<div className='overlay'>
 				<span></span>
 				<span></span>
 			</div>
-			<Navbar />
 			<div>
-				<div className='px-8'>
-					<CallToAction />
-					<SubscribeForm />
-				</div>
-				<div className='pt-20 flex justify-center'>
-					<Image src={headline} alt='Une personne devant un ordinateur' />
+				<Navbar />
+				<div>
+					<div className='px-8'>
+						<CallToAction />
+						<SubscribeForm />
+					</div>
+					<div className='pt-20 flex justify-center'>
+						<Image src={headline} alt='Une personne devant un ordinateur' />
+					</div>
 				</div>
 			</div>
 		</header>
