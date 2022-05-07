8fd32769ce1d3b050460267f8af89f39dde937fc
Adeline <adeline.sire@gmail.com>
2022-05-07 08:59:57 +0200

    Change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
 						href='https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=Rubik:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap'
 						rel='stylesheet'
 					/>
+					<link rel='icon' href='/favicon.ico' />
 				</Head>
 				<body>
 					<Main />
