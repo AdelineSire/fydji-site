5357ad9f704cc9626a2b225434207c74328b8cb6
Adeline <adeline.sire@gmail.com>
2022-05-20 14:32:32 +0200

    Adds beta to logo

--- components/logo/Logo.js
+++ components/logo/Logo.js
@@ -5,6 +5,7 @@ function Logo() {
 		<div className='flex flex-row items-center px-8 py-4 gap-2'>
 			<LogoSvg />
 			<h1 className='text-3xl font-logo text-white'>Fydji</h1>
+			<span className='self-end font-bold text-white text-[0.6em]'>BETA</span>
 		</div>
 	);
 }
